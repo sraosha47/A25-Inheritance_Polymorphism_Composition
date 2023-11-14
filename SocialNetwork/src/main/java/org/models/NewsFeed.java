bc6a289931c39fb5d946b7b0236b5751dd6f5215
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-14 15:51:53 +0100

    created a new project and copied the classes from the source code

---
+++ SocialNetwork/src/main/java/org/models/NewsFeed.java
@@ -0,0 +1,41 @@
+package org.models;
+
+import java.util.ArrayList;
+
+public class NewsFeed {
+
+
+    private ArrayList<MessagePost> messages;
+    private ArrayList<PhotoPost> photos;
+
+
+    public NewsFeed() {
+
+        messages = new ArrayList<>();
+        photos = new ArrayList<>();
+    }
+
+
+    public void addMessagePost(MessagePost message) {
+        messages.add(message);
+    }
+
+    public void addPhotoPost(PhotoPost photo) {
+        photos.add(photo);
+    }
+
+    public void show() {
+
+        for(MessagePost message : messages) {
+            message.display();
+            System.out.println();
+        }
+
+        for(PhotoPost photo : photos) {
+            photo.display();
+            System.out.println();
+        }
+
+    }
+
+}
